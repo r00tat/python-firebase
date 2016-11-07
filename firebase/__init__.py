d5736b00340d12e9bda50ebc3018ec0453d72d0e
Paul Woelfel <paul.woelfel@zirrus.eu>
2016-11-07 14:06:33 +0100

    version 1.1.0

--- firebase/__init__.py
+++ firebase/__init__.py
@@ -2,7 +2,7 @@
 """ firebase client library """
 
 MAJOR_VERSION = 1
-MINOR_VERSION = 0
-PATCH_LEVEL = 1
+MINOR_VERSION = 1
+PATCH_LEVEL = 0
 
 __version__ = "{}.{}.{}".format(MAJOR_VERSION, MINOR_VERSION, PATCH_LEVEL)
